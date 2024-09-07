9c70676ed0627d8bec18b40e6bc034de61cd218c
Ritaakterlaboni <ritaakterlaboni4@gmail.com>
2024-09-07 21:47:19 +0600

    here is for loop and odd ,even numbers

---
+++ for_loop_cls_20/for_loop/js/script.js
@@ -0,0 +1,12 @@
+
+//for loop start
+//even for loop start
+
+for (let i = 1; i <= 100; i +=2 ){
+    console.log(i);
+}
+
+//odd for loop start
+for (let i = 2; i = 100; i += 2){
+    console.log(i);
+}
